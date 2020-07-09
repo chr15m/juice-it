7388b0bae84847aebdd4a180e9e816f921f2d14b
Chris McCormick <chris@mccormick.cx>
2020-07-09 19:01:54 +0800

    First animation - bouncey face.

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -4,8 +4,24 @@
 
 (defonce state (r/atom {}))
 
+(defn move [x y attrs]
+  (merge attrs {:style {:translate (str x "px " y "px")}}))
+
+(defn component-card-click-bounce []
+  (let [on (r/atom false)]
+    (fn []
+      [:div.card
+       {:on-click #(reset! on true)}
+       [:svg.shadow (move 0 43 {:width 100 :height 100}) [:ellipse {:cx 50 :cy 50 :ry 5 :rx 30 :fill "#888" :opacity 0.5
+                                                                    :class (when @on "animate__animated animate__shadow__bounce")}]]
+       (if @on
+         [:i.twa.twa-zany-face.twa-5x {:class "animate__animated animate__bounce"
+                                       :on-animation-end #(reset! on false)}]     
+         [:i.twa.twa-grinning-face.twa-5x])])))
+
 (defn component-main [state]
-  [:div "Hello world."])
+  [component-card-click-bounce]
+  )
 
 (defn main []
   (print "hi"))
